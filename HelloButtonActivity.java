ff24b71756e17c445fd458e37ee39923a0f96388
Charif Mahmoudi <charif.mahmoudi@sfr.fr>
2017-10-19 04:27:35 +0200

    Activity

---
+++ HelloButtonActivity.java
@@ -0,0 +1,97 @@
+/**
+ * A kind of Hello Example of using Button driver for toggling a LED with Android Things.
+ *
+ * This activity initialize an InputDriver to emit key events when the button GPIO pin state change
+ * and flip the state of the LED GPIO pin. Note that an LED and a push button switch to pins specified in {@link PiDefaults}
+ * are required.
+ */
+public class ButtonActivity extends Activity {
+    private static final String TAG = ButtonActivity.class.getSimpleName();
+
+    private Gpio mLedGpio;
+    private ButtonInputDriver mButtonInputDriver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Starting ButtonActivity");
+
+        PeripheralManagerService pioService = new PeripheralManagerService();
+        try {
+            Log.i(TAG, "Configuring GPIO pins");
+            mLedGpio = pioService.openGpio(PiDefaults.getGPIOForLED());
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+
+            Log.i(TAG, "Registering button driver");
+            // Initialize and register the InputDriver that will emit SPACE key events
+            // on GPIO state changes.
+            mButtonInputDriver = new ButtonInputDriver(
+                    PiDefaults.getGPIOForButton(),
+                    Button.LogicState.PRESSED_WHEN_LOW,
+                    KeyEvent.KEYCODE_SPACE);
+            mButtonInputDriver.register();
+        } catch (IOException e) {
+            Log.e(TAG, "Error configuring GPIO pins", e);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_SPACE) {
+            // Turn on the LED
+            setLedValue(true);
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_SPACE) {
+            // Turn off the LED
+            setLedValue(false);
+            return true;
+        }
+
+        return super.onKeyUp(keyCode, event);
+    }
+
+    /**
+     * Update the value of the LED output.
+     */
+    private void setLedValue(boolean value) {
+        try {
+            mLedGpio.setValue(value);
+        } catch (IOException e) {
+            Log.e(TAG, "Error updating GPIO value", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        if (mButtonInputDriver != null) {
+            mButtonInputDriver.unregister();
+            try {
+                mButtonInputDriver.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing Button driver", e);
+            } finally{
+                mButtonInputDriver = null;
+            }
+        }
+
+        if (mLedGpio != null) {
+            try {
+                mLedGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing LED GPIO", e);
+            } finally{
+                mLedGpio = null;
+            }
+            mLedGpio = null;
+        }
+    }
+}
